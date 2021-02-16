75bda60cfa2d14ffa029f232e2eba65aa6fc3394
Russell Goldin <Russ.Goldin@gmail.com>
2021-02-16 06:46:32 -0800

    add isActive to body init for *touch

--- controller/comms/messages/status/EquipmentStateMessage.ts
+++ controller/comms/messages/status/EquipmentStateMessage.ts
@@ -275,6 +275,7 @@ export class EquipmentStateMessage {
             let tbody = state.temps.bodies.getItemById(i, true);
             // If the body doesn't represent a spa then we set the type.
             tbody.type = cbody.type = i > 1 && !sys.equipment.shared ? 1 : 0;
+            cbody.isActive = true;
         }
         if (!sys.equipment.shared && !sys.equipment.dual) {
             sys.bodies.removeItemById(2);
