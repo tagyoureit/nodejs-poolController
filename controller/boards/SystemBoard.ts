3202767a0ff8c417fea43f3318df43005fda7e35
Robert Strouse <rstrouse@truepointsolutions.com>
2022-01-15 09:38:40 -0800

    Redirect IntelliChemState to the associated board.

--- controller/boards/SystemBoard.ts
+++ controller/boards/SystemBoard.ts
@@ -4412,7 +4412,7 @@ export class ChemControllerCommands extends BoardCommands {
             // So here is the thing.  If you have an OCP then the IntelliChem must be controlled by that.
             // the messages on the bus will talk back to the OCP so if you do not do this mayhem will ensue.
             if (type.name === 'intellichem')
-                await this.setIntelliChemAsync(data);
+                await sys.board.chemControllers.setIntelliChemAsync(data);
             else
                 await ncp.chemControllers.setControllerAsync(chem, data);
             return Promise.resolve(chem);
