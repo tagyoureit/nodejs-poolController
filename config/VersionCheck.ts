9e48a54fc86b601b260d3089373c89a1514b797f
Russell Goldin <Russ.Goldin@gmail.com>
2022-02-08 12:47:11 -0800

    Check for docker env variables: 1. SOURCE_BRANCH 2. SOURCE_COMMIT

--- config/VersionCheck.ts
+++ config/VersionCheck.ts
@@ -39,27 +39,42 @@ class VersionCheck {
         if (typeof state.appVersion.nextCheckTime === 'undefined' || new Date() > new Date(state.appVersion.nextCheckTime)) setTimeout(() => { this.checkAll(); }, 100);
     }
     public checkGitLocal() {
+        let env = process.env;
         // check local git version
         try {
-            let res = execSync('git rev-parse --abbrev-ref HEAD', { stdio: 'pipe' });
-            let out = res.toString().trim();
+            let out: string;
+            if (typeof env.SOURCE_BRANCH !== 'undefined') 
+            {
+                out = `${env.SOURCE_BRANCH} - From environment variable`; // check for docker variable
+            }
+            else {
+                let res = execSync('git rev-parse --abbrev-ref HEAD', { stdio: 'pipe' });
+                out = res.toString().trim();
+            }
             logger.info(`The current git branch output is ${out}`);
             switch (out) {
-                case 'fatal':
-                case 'command':
-                    state.appVersion.gitLocalBranch = '--';
-                    break;
-                default:
-                    state.appVersion.gitLocalBranch = out;
-            }
+            case 'fatal':
+            case 'command':
+                state.appVersion.gitLocalBranch = '--';
+                break;
+            default:
+                state.appVersion.gitLocalBranch = out;
+        }
         }
         catch (err) {
             state.appVersion.gitLocalBranch = '--';
             logger.warn(`Unable to retrieve local git branch.  ${err}`);
         }
         try {
-            let res = execSync('git rev-parse HEAD', { stdio: 'pipe' });
-            let out = res.toString().trim();
+            let out: string;
+            if (typeof env.SOURCE_COMMIT !== 'undefined') 
+            {
+                out = `${env.SOURCE_COMMIT} - From environment variable`; // check for docker variable
+            }
+            else {
+                let res = execSync('git rev-parse HEAD', { stdio: 'pipe' });
+                out = res.toString().trim();
+            }
             logger.info(`The current git commit output is ${out}`);
             switch (out) {
                 case 'fatal':
