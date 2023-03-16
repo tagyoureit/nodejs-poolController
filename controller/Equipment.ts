35bba2dea0546360e50259220c413f198bedd29e
Robert Strouse <rstrouse@truepointsolutions.com>
2023-03-16 10:16:20 -0700

    Fixed circular reference for equipment id verification of pumps.  #703

--- controller/Equipment.ts
+++ controller/Equipment.ts
@@ -1398,7 +1398,7 @@ export class PumpCollection extends EqItemCollection<Pump> {
         if (typeof add !== 'undefined' && add) return this.add(data || { id: this.data.length + 1, address: address });
         return this.createItem(data || { id: this.data.length + 1, address: address });
     }
-    public getNextEquipmentId(range?: EquipmentIdRange, exclude?: number[]): number { return this.getNextEquipmentId(typeof range === 'undefined' ? sys.board.equipmentIds.pumps : range, exclude); }
+    public getNextEquipmentId(range?: EquipmentIdRange, exclude?: number[]): number { return super.getNextEquipmentId(typeof range === 'undefined' ? sys.board.equipmentIds.pumps : range, exclude); }
 }
 export class Pump extends EqItem {
     public dataName = 'pumpConfig';
