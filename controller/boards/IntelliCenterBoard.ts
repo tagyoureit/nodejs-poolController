8dd6f3bbb33a8b3131660514e953837a3070dac1
Joseph <22131916+camaro4life18@users.noreply.github.com>
2026-05-03 18:53:22 -0400

    Fix: Correct Hayward Super Pump VS 700 maxSpeed to 3450 per manual

--- controller/boards/IntelliCenterBoard.ts
+++ controller/boards/IntelliCenterBoard.ts
@@ -85,7 +85,7 @@ export class IntelliCenterBoard extends SystemBoard {
             [100, { name: 'sf', desc: 'SuperFlo VS', hasAddress: false, maxCircuits: 8, maxRelays: 4, equipmentMaster: 1, maxSpeeds: 4, relays: [{ id: 1, name: 'Program #1' }, { id: 2, name: 'Program #2' }, { id: 3, name: 'Program #3' }, { id: 4, name: 'Program #4' }] }],
             [101, { name: 'hwrly', desc: 'Hayward Relay VS', hasAddress: false, maxCircuits: 8, maxRelays: 4, equipmentMaster: 1, maxSpeeds: 8, relays: [{ id: 1, name: 'Step #1' }, { id: 2, name: 'Step #2' }, { id: 3, name: 'Step #3' }, { id: 4, name: 'Pump On' }] }],
             [102, { name: 'hwvs', desc: 'Hayward Eco/TriStar VS', minSpeed: 450, maxSpeed: 3450, maxCircuits: 8, hasAddress: true, equipmentMaster: 1 }],
-            [103, { name: 'hwsp', desc: 'Hayward Super Pump VS 700', minSpeed: 450, maxSpeed: 3000, maxCircuits: 8, hasAddress: true, equipmentMaster: 1 }]
+            [103, { name: 'hwsp', desc: 'Hayward Super Pump VS 700', minSpeed: 450, maxSpeed: 3450, maxCircuits: 8, hasAddress: true, equipmentMaster: 1 }]
         ]);
         // RSG - same as systemBoard definition; can delete.
         this.valueMaps.heatModes = new byteValueMap([
